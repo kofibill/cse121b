73d4405a1645d1cd3f265aabff984e391b392c10
kofibill <jessekim064@gmail.com>
2023-11-26 03:44:48 -0700

    added new codes

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -2,10 +2,16 @@
 
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
-
+const numbers =[1,2,3,4,5,6,7,8,9,10,11,12,13];
+ 
+const add= (number1,number2) =>{
+      return number1+number2;
+}
 
 /* Function Expression - Subtract Numbers */
-
+const subtract =(number1,number2)=>{
+    return number1-number2;
+}
 
 /* Arrow Function - Multiply Numbers */
 
@@ -18,13 +24,17 @@
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
-
+document.querySelector('#array').innerHTML = numbers.map((value)=>{
+    return value;
+});
 /* Output Odds Only Array */
-
+document.querySelector('#odds').innerHTML = numbers.filter((value)=>{
+    if((value%2)>0) return value;
 /* Output Evens Only Array */
-
+document.getElementById('evens').innerHTML=  numbers.filter((myvalue)=>{if( (myvalue%2)==0)  return myvalue;});
 /* Output Sum of Org. Array */
-
+document.getElementById('sumOfArray').innerHTML = numbers.reduce( (val,sumatory) =>{ return val+sumatory;});
 /* Output Multiplied by 2 Array */
-
+document.querySelector('#multiplied').innerHTML = numbers.map((val)=>{ return val*2;})
 /* Output Sum of Multiplied by 2 Array */
+document.getElementById('sumOfMultiplied').innerHTML = numbers.map((value)=>value*2).reduce( (val,suma)=>val + suma);
---
+++ scripts/w04-task.js
@@ -0,0 +1,59 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = { name:'William Osiakwan',
+                photo:'images/dv photo.jpg',
+                favoriteFoods:['Rice','Steak','Fufu with meat','Jollof Rice'],
+                hobbies:['Playing video games','Programming'],
+                placesLived:[],
+    };
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push({place:'Calabar, Nigeria', length:'6 month'});
+myProfile.placesLived.push({place:'Oron, Nigeria', length:'1 year'});
+myProfile.placesLived.push({place:'Uyo, Nigeria', length:'6 months'});
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+document.querySelector('#name').textContent= myProfile.name;
+/* Photo with attributes */
+document.querySelector('#photo').setAttribute('src', myProfile.photo);
+document.querySelector('#photo').setAttribute('alt', `Photo of ${myProfile.name}`);
+
+/* Favorite Foods List*/
+const favoriteFoodsList = document.querySelector('#favorite-foods'); 
+
+myProfile.favoriteFoods.forEach((food) => { 
+  const listItem = document.createElement('li'); 
+  listItem.textContent = food; 
+  favoriteFoodsList.appendChild(listItem); 
+});
+
+/* Hobbies List */
+const hobbies = document.querySelector('#hobbies');
+myProfile.hobbies.map((myhobby)=>{
+  const myelement = document.createElement('li');
+  myelement.textContent=myhobby;
+  hobbies.appendChild(  myelement);
+}); 
+
+/* Places Lived DataList */
+const placesLived = document.querySelector('#places-lived');
+const myPlaces = myProfile.placesLived;
+myPlaces.map((place) => {
+
+  const myElementPlace= document.createElement('dt');
+  myElementPlace.textContent=place.place;
+
+  const myElementlength= document.createElement('dd');
+  myElementlength.textContent=place.length;
+
+  placesLived.appendChild(myElementPlace);
+  placesLived.appendChild(myElementlength);
+ 
+});
+
+
