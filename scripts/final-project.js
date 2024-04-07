b4da897346e9da3a195783ecb0a5819e6de47a6a
kofibill <jessekim064@gmail.com>
2024-04-07 13:03:48 -0600

    updates

--- scripts/final-project.js
+++ scripts/final-project.js
@@ -5,6 +5,7 @@ async function fetchData(){
 
     try{
         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        
         // fetch data from the pokeman api
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
