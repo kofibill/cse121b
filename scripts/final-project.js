e07b124c743505cff2dac166dd18f7d1a944756c
kofibill <jessekim064@gmail.com>
2024-04-06 22:19:56 -0600

    final project done

---
+++ scripts/final-project.js
@@ -0,0 +1,29 @@
+fetchData();
+
+
+async function fetchData(){
+
+    try{
+        const pokemonName = document.getElementById("pokemonName").value;
+        // fetch data from the pokeman api
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+
+        // conditional branch to handle the response.
+        if(!response.ok){
+            throw new Error(`Could not find this type of resourse`);
+        }
+
+        // converting the response to a json file
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+        
+    }
+    catch(error){
+        console.error(error); 
+   }
+}
